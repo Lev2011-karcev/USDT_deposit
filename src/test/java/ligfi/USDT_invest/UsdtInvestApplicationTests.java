68451c91d9b32a26e301d53d9d3767eeb2b32084
levchik2011 <lev.kartsev.00@mail.ru>
2025-07-26 21:30:46 +0500

    usdt deposit proj

---
+++ src/test/java/ligfi/USDT_invest/UsdtInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package ligfi.USDT_invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsdtInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
